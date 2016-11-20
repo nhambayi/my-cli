7ed459eaf1de0c0e60c844c5d470d972029ac3b2
noah hambayi <nhambayi@prescribewellness.com>
2016-11-20 00:23:31 -0800

    Initial commit

---
+++ tests/testClass.js
@@ -0,0 +1,5 @@
+export class myClass {
+    render() {
+        return null;
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {  
+  target: 'node',
+  entry: './src/index.ts',
+  output: {
+    filename: './dist/index.js'
+  },
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+  },
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: 'ts-loader' }
+    ]
+  }
+}
